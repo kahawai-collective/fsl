72fdbf289e2471dc1780d5fdbe0170e108e109d4
Nokome Bentley <nokome.bentley@trophia.com>
2013-04-09 17:09:04 +1200

    tidies up Managaement::Procedures

--- fisl/management/procedures/evaluation.hpp
+++ fisl/management/procedures/evaluation.hpp
@@ -9,13 +9,14 @@ class Evaluation {
 public:
 
     int run(void){
-		static_cast<Derived*>(this)->priors();
-		static_cast<Derived*>(this)->posteriors();
-		
+        static_cast<Derived*>(this)->priors();
+        static_cast<Derived*>(this)->posteriors();
+        
         return 0;
     }
-};
+    
+}; //end class Evaluation
 
-}
-}
-}
+} //end namespace Fisl
+} //end namespace Management
+} //end namespace Procedures
