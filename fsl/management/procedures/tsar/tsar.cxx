ad53df770cdd50fd099070a0cb0af1514abefd27
Nokome Bentley <nokome.bentley@trophia.com>
2015-02-16 10:34:25 +1300

    Refactors TSAR into a module

---
+++ fsl/management/procedures/tsar/tsar.cxx
@@ -0,0 +1,13 @@
+#include <boost/test/unit_test.hpp>
+
+#include <fsl/management/procedures/tsar.hpp>
+using Fsl::Management::Procedures::TSAR;
+
+BOOST_AUTO_TEST_SUITE(tsar)
+ 
+BOOST_AUTO_TEST_CASE(construct){
+	TSAR i;
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+ 
