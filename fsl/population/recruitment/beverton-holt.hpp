5fcb51a52e45f95ccc101c3b4f5052d87bc4e7ed
Nokome Bentley <nokome.bentley@trophia.com>
2015-09-30 13:47:16 +1300

    Make BevertonHolt a structure

--- fsl/population/recruitment/beverton-holt.hpp
+++ fsl/population/recruitment/beverton-holt.hpp
@@ -1,14 +1,19 @@
 #pragma once
 
+#include <boost/format.hpp>
+
+#include <stencila/structure.hpp>
+using Stencila::Structure;
+
 namespace Fsl {
 namespace Population {
 namespace Recruitment {
 
 /**
 Beverton-Holt stock recruitment relationship parameterised using steepness and
-pristine recruitment
+pristine stock and recruitment levels
 */
-class BevertonHolt {
+class BevertonHolt : public Structure<BevertonHolt> {
 public:
 
     double r0;
@@ -16,9 +21,15 @@ public:
     double steepness;
 
     void check(void) const {
-        if(not std::isfinite(r0) or r0 <= 0) throw std::runtime_error(str(boost::format("`BevertonHolt::r0` has invalid value <%s>")%r0));
-        if(not std::isfinite(s0) or s0 <= 0) throw std::runtime_error(str(boost::format("`BevertonHolt::s0` has invalid value <%s>")%s0));
-        if(not std::isfinite(steepness) or steepness<=0.2 or steepness>1) throw std::runtime_error(str(boost::format("`BevertonHolt::steepness` has invalid value <%s>")%steepness));
+        if(not std::isfinite(r0) or r0 <= 0){
+            throw std::runtime_error(str(boost::format("`BevertonHolt::r0` has invalid value.\n  r0: %s")%r0));
+        }
+        if(not std::isfinite(s0) or s0 <= 0){
+            throw std::runtime_error(str(boost::format("`BevertonHolt::s0` has invalid value.\n s0: %s")%s0));
+        }
+        if(not std::isfinite(steepness) or steepness<=0.2 or steepness>1){
+            throw std::runtime_error(str(boost::format("`BevertonHolt::steepness` has invalid value.\n steepness: %s")%steepness));
+        }
     }
 
     double alpha(void) const {
@@ -33,6 +44,14 @@ public:
         return 4*steepness*r0*stock/((5*steepness-1)*stock+s0*(1-steepness));
     }
 
+    template<class Mirror>
+    void reflect(Mirror& mirror){
+        mirror
+            .data(r0,"r0")
+            .data(s0,"s0")
+            .data(steepness,"steepness")
+        ;
+    }
 };
 
 }
