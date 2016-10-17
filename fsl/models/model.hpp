c4987b372becab817c5914e2849b634f228cff66
Nokome Bentley <nokome@trophia.com>
2016-10-17 19:50:49 +1300

    Adds base model class

---
+++ fsl/models/model.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <stencila/structure.hpp>
+using Stencila::Structure;
+
+namespace Fsl {
+namespace Models {
+
+template<
+	class Derived
+>
+class Model : public Structure<Derived> {
+public:
+
+	typedef Structure<Derived> Base;
+
+	void startup(void){
+		read();
+	}
+
+	void shutdown(void){
+		write();
+	}
+
+	void read(const std::string& path = "input/model.json"){
+		Base::read(path);
+	}
+
+	void write(const std::string& path = "output/model.json"){
+		Base::write(path);
+	}
+
+	void run(void){
+	}
+};
+
+}
+}
