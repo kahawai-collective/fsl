3895061d4cf4af56a6fa9269a459142c22818ccf
Nokome Bentley <nokome@trophia.com>
2016-10-17 15:50:51 +1300

    Rename steepness -> h

--- fsl/population/recruitment/beverton-holt.hpp
+++ fsl/population/recruitment/beverton-holt.hpp
@@ -18,7 +18,7 @@ public:
 
     double r0;
     double s0;
-    double steepness;
+    double h;
 
     void check(void) const {
         if(not std::isfinite(r0) or r0 <= 0){
@@ -27,21 +27,21 @@ public:
         if(not std::isfinite(s0) or s0 <= 0){
             throw std::runtime_error(str(boost::format("`BevertonHolt::s0` has invalid value.\n s0: %s")%s0));
         }
-        if(not std::isfinite(steepness) or steepness<=0.2 or steepness>1){
-            throw std::runtime_error(str(boost::format("`BevertonHolt::steepness` has invalid value.\n steepness: %s")%steepness));
+        if(not std::isfinite(h) or h<=0.2 or h>1){
+            throw std::runtime_error(str(boost::format("`BevertonHolt::h` has invalid value.\n h: %s")%h));
         }
     }
 
     double alpha(void) const {
-        return 4*r0*steepness/(5*steepness-1);
+        return 4*r0*h/(5*h-1);
     }
 
     double beta(void) const {
-        return -(s0*steepness-s0)/(5*steepness-1);
+        return -(s0*h-s0)/(5*h-1);
     }
 
     double operator()(const double& stock) {
-        return 4*steepness*r0*stock/((5*steepness-1)*stock+s0*(1-steepness));
+        return 4*h*r0*stock/((5*h-1)*stock+s0*(1-h));
     }
 
     template<class Mirror>
@@ -49,7 +49,7 @@ public:
         mirror
             .data(r0,"r0")
             .data(s0,"s0")
-            .data(steepness,"steepness")
+            .data(h,"h")
         ;
     }
 };
