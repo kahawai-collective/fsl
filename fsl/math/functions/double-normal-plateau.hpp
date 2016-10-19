58b4be12f98085ff7b6e17b23c071a7f9e244331
Nokome Bentley <nokome@trophia.com>
2016-10-19 17:04:43 +1300

    Add reflection

--- fsl/math/functions/double-normal-plateau.hpp
+++ fsl/math/functions/double-normal-plateau.hpp
@@ -16,6 +16,16 @@ public:
     double steepness_1;
     double steepness_2;
 
+    template<class Mirror>
+    void reflect(Mirror& mirror){
+        mirror
+            .data(inflection_1, "inflection_1")
+            .data(inflection_2_delta, "inflection_2_delta")
+            .data(steepness_1, "steepness_1")
+            .data(steepness_2, "steepness_2")
+        ;
+    }
+
     double value(const double& x) const {
         if(x<=inflection_1) return std::pow(2,-std::pow((x-inflection_1)/steepness_1,2));
         else if(x>inflection_1+inflection_2_delta) return std::pow(2,-std::pow((x-(inflection_1+inflection_2_delta))/steepness_2,2));
