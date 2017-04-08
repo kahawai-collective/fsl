fdf320ea81548257e1f79c6cdb672bc2f61df9dc
Nokome Bentley <nokome@trophia.com>
2017-04-08 18:22:10 +1200

    Remove unused stuff

--- fsl/monitoring/cpue.hpp
+++ fsl/monitoring/cpue.hpp
@@ -5,22 +5,6 @@
 namespace Fsl {
 namespace Monitoring {
 
-class CpueRecord : public Structure< CpueRecord > {
-public:
-
-	double raw;
-	double normalised;
-
-	template<class Mirror>
-    void reflect(Mirror& mirror) {
-        mirror
-            .data(raw, "raw")
-            .data(normalised, "normalised")
-        ;
-    }
-
-};
-
 template<class Time>
 class Cpue : public Structure< Cpue<Time> > {
 public:
@@ -35,14 +19,12 @@ public:
     }
 
 	void initialise() {
-
 	}
 
 	template<class Population, class Harvesting>
 	void update(uint time, const Population& population, const Harvesting& harvesting) {
 		series(time) = harvesting.biomass_selected(population);
 	}
-
 };
 
 }
