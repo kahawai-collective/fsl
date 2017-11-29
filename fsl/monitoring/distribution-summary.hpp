4b14bb016fa446aac04140c62463608ea09283e8
Nokome Bentley <nokome@trophia.com>
2017-11-29 17:27:27 +1300

    Add calculation of more stats

--- fsl/monitoring/distribution-summary.hpp
+++ fsl/monitoring/distribution-summary.hpp
@@ -8,9 +8,10 @@ namespace Monitoring {
 class DistributionSummary : public Structure <DistributionSummary> {
 public:
 
-	double mean;
-	double median;
-    double p10;
+	double mean = NAN;
+	double median = NAN;
+    double p10 = NAN;
+    double mrad = NAN;
 
 	template<class Mirror>
     void reflect(Mirror& mirror) {
@@ -18,17 +19,20 @@ public:
             .data(mean, "mean")
             .data(median, "median")
             .data(p10, "p10")
+            .data(mrad, "mrad")
         ;
     }
 
     template<class Dimension>
-    void calculate(Array<double, Dimension> array) {
+    void calculate(const Array<double, Dimension>& array) {
     	double cumul = 0;
     	double sum = 0;
     	double sumprod = 0;
+        double last = -1;
     	uint n = 0;
     	median = 0;
         p10 = 0;
+        mrad = 0;
     	for (auto level : Dimension::levels) {
     		auto value = level.index();
     		auto prop = array(level);
@@ -37,9 +41,15 @@ public:
     		sumprod += prop * value;
     		if (median == 0 and cumul >= 0.5) median = value;
             if (p10 == 0 and cumul >= 0.1) p10 = value;
+            if (last >=0.01 and prop>=0.01) {
+                mrad += std::fabs(prop-last)/(prop+last)*0.5;
+            }
+            last = prop;
+
     		n++;
     	}
     	mean = sumprod/sum;
+        mrad /=n;
     }
 
 };
